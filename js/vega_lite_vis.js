2bf6b05e1c6ee43989f77442ad5fdd78c2c28cb9
milnay <126925768+milnay@users.noreply.github.com>
2024-09-26 21:08:58 +1000

    Added js file that embeds VG

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg1 = "js/critical_mines_map.vg.json"
+
+vegaEmbed('#critical_mines_map', vg1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
